415733c73f5f5d695c8e4dc3e24bdade57237615
mamanssuparman <mamanssuparman@gmail.com>
2023-12-06 22:26:05 +0700

    REPAIR FEATURE SAMBUTAN KEPALA SEKOLAH

--- resources/views/frontend/includes/sectionsambutankepalasekolah.blade.php
+++ resources/views/frontend/includes/sectionsambutankepalasekolah.blade.php
@@ -1,14 +1,14 @@
 <section class="py-6 md:py-0 sm:py-0">
     <div class="flex lg:h-[387px] bg-blue-900 w-full">
-        <div class="flex flex-col w-full lg:flex-row md:flex-col">
+        <div class="flex flex-col items-center w-full lg:flex-row md:flex-col">
             <!-- Start Kiri  -->
             <div class="flex px-10 lg:w-full md:w-full sm:w-full">
                 <div class="flex flex-col justify-center px-4 py-4">
                     <div class="flex pb-4 font-mono text-3xl font-semibold text-white" id="body_judul_sambutan">
-                        
+
                     </div>
                     <div class="text-sm text-justify text-white" id="body_isi_sambutan">
-                        
+
                     </div>
                 </div>
             </div>
@@ -16,7 +16,7 @@
             <!-- Start Kanan  -->
             <div class="flex  lg:w-[700px] md:w-full sm:w-full justify-center">
                 <div class="px-10 py-10">
-                    <img src="" alt=""
+                    <img src="" alt="" width="200px"
                         class="border border-gray-800 rounded-md shadow-lg border-spacing-4" id="body_foto_kepala">
                 </div>
             </div>
