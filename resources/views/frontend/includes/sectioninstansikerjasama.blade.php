df6c4b29dc40417355cb7d62acb714dba2f7e2d5
mamanssuparman <mamanssuparman@gmail.com>
2023-12-06 22:24:09 +0700

    REPAIR FEATURE MITRA

--- resources/views/frontend/includes/sectioninstansikerjasama.blade.php
+++ resources/views/frontend/includes/sectioninstansikerjasama.blade.php
@@ -1,24 +1,13 @@
 <section class="px-6 py-4 bg-blue-800">
-    <div
-        class="flex justify-between gap-4 py-10 overflow-x-auto transition lg:flex-row md:flex-col sm:flex-col scroll-smooth">
-        <div class="flex gap-4 lg:flex-row md:flex-col">
-            <div class="flex w-48 h-full grid-cols-1 px-4 py-4 transition bg-white shadow-lg hover:scale-105">
-                <img src="/images/Logo-Provinsi-Jawa-Barat-e1676964438498.png" alt="">
-            </div>
-            <div class="flex w-48 h-full grid-cols-1 px-4 py-4 transition bg-white shadow-lg hover:scale-105">
-                <img src="/images/logo disdik jawa barat.png" alt="">
-            </div>
-            <div class="flex w-48 h-full grid-cols-1 px-4 py-4 transition bg-white shadow-lg hover:scale-105">
-                <img src="/images/logo cabang dinas wil xiii.png" alt="">
-            </div>
-            <div class="flex w-48 h-full grid-cols-1 px-4 py-4 transition bg-white shadow-lg hover:scale-105 ">
-                <img src="/images/logo merdeka belajar.png" alt="">
-            </div>
-            <div class="flex w-48 h-full grid-cols-1 px-4 py-4 transition bg-white shadow-lg hover:scale-105 ">
-                <img src="/images/logo vokasi.png" alt="">
-            </div>
-            <div class="flex w-48 h-full grid-cols-1 px-4 py-4 transition bg-white shadow-lg hover:scale-105 ">
-                <img src="/images/logo SMK Bisa.jpg" alt="">
+    <div class="flex justify-center">
+        <div
+            class="flex justify-between gap-4 py-10 overflow-x-auto transition lg:flex-row md:flex-col sm:flex-col scroll-smooth">
+            <div class="flex gap-4 lg:flex-row md:flex-col">
+                @foreach ($dataMitra as $mitra)
+                <div class="flex h-full grid-cols-1 px-4 py-4 transition duration-500 ease-in-out bg-white rounded-md shadow-lg w-52 hover:scale-105">
+                    <img src="/images/{{ $mitra->picture }}" alt="" title="{{ $mitra->namamitra }}">
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
