2b21b8a86fd14eab84bf502e016888923b73c84a
mamanssuparman <mamanssuparman@gmail.com>
2023-12-10 11:40:53 +0700

    Repair page FE GTK

--- app/Http/Controllers/Users/GtkController.php
+++ app/Http/Controllers/Users/GtkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Users;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,9 +10,14 @@ class GtkController extends Controller
 {
     public function index(Request $request)
     {
+        $dataGtk = User::where('jabatan', '!=', 'Kepala Sekolah')->where('statususers', 'Active')->orderBy('nama', 'asc');
+        if(request('search')){
+            $dataGtk->where('nama', 'like', '%'.request('search').'%');
+        }
         $data = [
-            'title'     => 'SMK Negeri 3 Banjar | Profile'
+            'title'     => 'SMK Negeri 3 Banjar | Profile',
+            'dataGtk'   => $dataGtk->paginate(12)
         ];
-        return view('frontend.pages.gtk.index', compact('data'));
+        return view('frontend.pages.gtk.index', $data);
     }
 }
--- resources/views/frontend/pages/gtk/index.blade.php
+++ resources/views/frontend/pages/gtk/index.blade.php
@@ -1,151 +1,58 @@
 @extends('frontend.layouts.layoutusers')
 @push('jscssexternal')
-<script src="/assetsusers/jquery-3.7.0.js"></script>
-<script src="/assetsusers/jquery.dataTables.min.js"></script>
-<script src="/assetsusers/dataTables.tailwindcss.min.js"></script>
-<script src="https://cdn.tailwindcss.com"></script>
-<link rel="stylesheet" href="/assetsusers/dataTables.tailwindcss.min.css">
+    <script src="/assetsusers/jquery-3.7.0.js"></script>
 @endpush
 @section('content')
     <!-- nav title -->
     <section class="mt-24">
         <div class="p-5 text-center bg-blue-900">
-            <h1 class="text-2xl font-semibold text-white">Guru dan Tenaga Pendidik</h1>
+            <h1 class="text-2xl font-semibold text-white">GTK</h1>
         </div>
     </section>
     <!--end nav title -->
 
 
-    <!-- Start GTK detail -->
-    <div class="flex flex-row">
-        <div class="w-full px-10 py-10 overflow-x-auto snap-x scroll-smooth scrollbar-hidden">
-            <table id="example" class="display" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>-</th>
-                        <th>NIP</th>
-                        <th>NUPTK</th>
-                        <th>Nama</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>
-                            <img src="/images/contohfoto.jpg" alt="" class="w-40 rounded-md shadow-md">
-                        </td>
-                        <td>12099900</td>
-                        <td>9898980989</td>
-                        <td>Nama Pegawai, S.Pd</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>
-                            <img src="/images/contohfoto.jpg" alt="" class="w-40 rounded-md shadow-md">
-                        </td>
-                        <td>12099900</td>
-                        <td>9898980989</td>
-                        <td>Nama Pegawai, S.Pd</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>
-                            <img src="/images/contohfoto.jpg" alt="" class="w-40 rounded-md shadow-md">
-                        </td>
-                        <td>12099900</td>
-                        <td>9898980989</td>
-                        <td>Nama Pegawai, S.Pd</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>
-                            <img src="/images/contohfoto.jpg" alt="" class="w-40 rounded-md shadow-md hover:cursor-pointer" data-modal-target="default-modal" data-modal-toggle="default-modal">
-                        </td>
-                        <td>12099900</td>
-                        <td>9898980989</td>
-                        <td>Nama Pegawai, S.Pd</td>
-                    </tr>
-
-
-                </tbody>
-            </table>
-        </div>
-
-    </div>
-    <!-- end Article detail -->
-
-    {{-- Start Modal View Detail GTK --}}
-    <div id="default-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full max-w-2xl max-h-full">
-            <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow ">
-                <!-- Modal header -->
-                <div class="flex items-start justify-between p-4 border-b rounded-t ">
-                    <h3 class="text-xl font-semibold text-gray-900 ">
-                        Detail Biodata GTK
-                    </h3>
-                    <button type="button" class="inline-flex items-center justify-center w-8 h-8 ml-auto text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
+    <!-- Data GTK -->
+    <section>
+        <div class="flex justify-center w-full mt-12">
+            <div class="w-3/4">
+                <div class="flex">
+                    <div class="flex items-center w-1 h-10 bg-gray-800 rounded-full ">
+                    </div>
+                    <div>
+                        <h1 class="flex items-center pl-4 text-xl font-semibold border-gray-900 ">Data GTK</h1>
+                    </div>
                 </div>
-                <!-- Modal body -->
-                <div class="p-6 space-y-6">
-                    <div class="flex flex-col">
-                        <div>
-                            <div class="flex flex-row w-full">
-                                <div class="flex w-full px-4">NIP</div>
-                                <div class="flex w-full px-4">Kanan</div>
+                <form action="" method="get" class="mt-8">
+                    <input type="search" class="w-1/2 rounded-lg text-slate-900" placeholder="Search..." name="search" value="{{ request('search') }}">
+                </form>
+                <div class="mt-5">
+                    <div class="flex flex-wrap items-start mt-12">
+                        @if (count($dataGtk))
+                            @foreach ($dataGtk as $gtk)
+                            <div class="w-full p-1 pt-8 md:w-1/6 lg:w-1/6">
+                                <div class="overflow-hidden rounded-lg aspect-w-16">
+                                    <img src="/images/{{ $gtk->photos }}" class="object-cover w-full h-full max-w-md transition ease-out sm:h-56 hover:scale-105 hover:rounded-none" alt="">
+                                </div>
+                                    <p class="font-semibold tracking-tight text-center text-gray-900">{{ $gtk->nama }}</p>
                             </div>
-                        </div>
-                        <div>
-                            <div class="flex flex-row w-full">
-                                <div class="flex w-full px-4">NUPTK</div>
-                                <div class="flex w-full px-4">Kanan</div>
+                            @endforeach
+                        @else
+                            <div class="w-full p-1 pt-8 md:w-1/3">
+                                <p class="font-semibold tracking-tight text-gray-900">Data GTK tidak ditemukan.!</p>
                             </div>
-                        </div>
-                        <div>
-                            <div class="flex flex-row w-full">
-                                <div class="flex w-full px-4">Nama</div>
-                                <div class="flex w-full px-4">Kanan</div>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="flex flex-row w-full">
-                                <div class="flex w-full px-4">Mapel Ajar</div>
-                                <div class="flex w-full px-4">Kanan</div>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="flex flex-row w-full">
-                                <div class="flex w-full px-4">Jabatan</div>
-                                <div class="flex w-full px-4">Guru</div>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="flex flex-row w-full">
-                                <div class="flex w-full px-4">Tugas Tambahan</div>
-                                <div class="flex w-full px-4">Guru</div>
-                            </div>
-                        </div>
+                        @endif
+
 
                     </div>
                 </div>
-                <!-- Modal footer -->
-                <div class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b ">
-                    <button data-modal-hide="default-modal" type="button" class="px-4 py-2 text-white bg-green-700 rounded-md ">Close</button>
+                <div class="mt-12 mb-12">
+                    {{ $dataGtk->links() }}
                 </div>
-            </div>
         </div>
-    </div>
+        </div>
+    </section>
+
 
     {{-- End Modal View Detail GTK --}}
 @endsection
-@push('jsexternal')
-<script type="text/javascript">
-    $('#example').DataTable();
-</script>
-@endpush
